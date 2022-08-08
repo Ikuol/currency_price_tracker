7835854b00f176bf007bf36605d80a435954e201
ikuol1 <damienssopolo@gmail.com>
2022-08-08 19:15:18 +0200

    +6

---
+++ currency.js
@@ -0,0 +1,25 @@
+let currency ={
+
+    FetchPrice: function(){
+        fetch(
+            'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd'
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayPrice(data));
+    },
+    displayPrice: function(data){
+        const btc = data.bitcoin.usd;
+        const eth = data.ethereum.usd;
+       
+        document.querySelector('.btc-price').innerText = `${btc}` + ' $';
+      
+        document.querySelector('.eth-price').innerText = `${eth}` + ' $';
+
+    }
+    
+};
+
+currency.FetchPrice();
+
+
+
