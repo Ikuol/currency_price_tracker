3de02c973179187dede47e88caae1ff589f17d9e
ikuol1 <damienssopolo@gmail.com>
2022-09-19 18:57:33 +0200

    +6

--- currency.js
+++ currency.js
@@ -5,9 +5,12 @@ let currency ={
             'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd'
         )
             .then((response) => response.json())
-            .then((data) => this.displayPrice(data));
+            .then((data) => {this.displayPrice(data)
+             setTimeout(currency.FetchPrice.bind(this),1500);
+            });
     },
     displayPrice: function(data){
+        
         const btc = data.bitcoin.usd;
         const eth = data.ethereum.usd;
        
